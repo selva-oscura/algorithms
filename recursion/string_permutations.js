e7d956c99a441f9c553ff3b874f0255caed218e9
Carol St.Louis <stlouis_c@yahoo.com>
2015-10-02 23:59:12 -0700

    added count of outputs (note: still have output duplicates where characters are repeated)

--- recursion/string_permutations.js
+++ recursion/string_permutations.js
@@ -2,11 +2,13 @@
 
 
 var permutateString = function(str){
+	var count = 0;
 	var output =[];
 	var prefix = "";
 	var permString  = function(str, prefix){
 		if(str===""){
 			output.push(prefix);
+			count++;
 			return;
 		}
 		for(var i = 0; i<str.length; i++){
@@ -16,14 +18,14 @@ var permutateString = function(str){
 			var remnant = str.slice(0,i)+str.slice(i+1, str.length);
 			permString(remnant, prefix+str[i]);
 		}
-		return output;
+		return {permutations: output, count: count};
 	}
 	return permString(str, prefix);
 }
 
 var string = "abc";
 var string2 = "abcd";
-var string3 = "abcde";
+var string3 = "aaaa";
 console.log(permutateString(string));
 console.log(permutateString(string2));
-console.log(permutateString(string3));
+console.log(permutateString(string3))
