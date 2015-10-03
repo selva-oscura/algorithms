b3c9b22ce22a40c03398ddf43e6dec0d3aa2ef62
Carol St.Louis <stlouis_c@yahoo.com>
2015-10-02 22:48:08 -0700

    iterated factorials -- added error checking for bad inputs

--- recursion/factorials.js
+++ recursion/factorials.js
@@ -10,7 +10,7 @@ var iteratedFactorial = function (n){
 		}
 		return output;
 	}
-	return "input must be an integer greater than or equal to 1"
+	return "input must be an integer greater than or equal to 1";
 }
 
 console.log(iteratedFactorial(2));
@@ -26,16 +26,22 @@ console.log(iteratedFactorial({a:'abc'}));
 
 
 var recursiveFactorial = function(n){
-	if(n===1 || n===0){
-		return 1;
+	if(n === Number(n) && n>=0 && n%1===0){	
+		if(n===1 || n===0){
+			return 1;
+		}
+		return n*recursiveFactorial(n-1);
 	}
-	return n*recursiveFactorial(n-1);
+	return "input must be an integer greater than or equal to 1";
 }
 
-console.log(recursiveFactorial(2))
-console.log(recursiveFactorial(4))
-console.log(recursiveFactorial(8))
-console.log(recursiveFactorial(13))
-console.log(recursiveFactorial(27))
-console.log(recursiveFactorial(0))
-console.log(recursiveFactorial(2))
+console.log(recursiveFactorial(2));
+console.log(recursiveFactorial(4));
+console.log(recursiveFactorial(8));
+console.log(recursiveFactorial(13));
+console.log(recursiveFactorial(27));
+console.log(recursiveFactorial(0));
+console.log(recursiveFactorial(1.2));
+console.log(recursiveFactorial(-4));
+console.log(recursiveFactorial('abc'));
+console.log(recursiveFactorial({a:'abc'}));
