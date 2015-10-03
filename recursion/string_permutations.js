f8085224cbd252658309d0b812960178c389e949
Carol St.Louis <stlouis_c@yahoo.com>
2015-10-03 00:11:02 -0700

    added a duplicate checker that ensures that duplicate answers are not output or counted; added discussion of time and space complexity

--- recursion/string_permutations.js
+++ recursion/string_permutations.js
@@ -1,14 +1,25 @@
 // for a given string, print all permutations of its constituent letters
 
+// Time complexity:
+//		for generation of permutations: n factorial
+//		for checking of permutations: proportional to number of permutations (hash used to check, so avoid checking each permutation against all of the already-generated permutations, as would be necessary if checking against an array)
+// Space complexity:
+//		twice a maximum of n factorial (n!), where n is the length of the string, if no duplicates (once for the output array and once for the hash table)
+//		if there are duplicate characters, then the space complexity is reduced divide n! (where n = length of string) by the k! of each character with k duplications  (e.g. a string of 20 with 1 character repeated twice and a second character repeated 3 times would be 20!/(2!*3!))
+// Could reduce space complexity by avoiding an output array and just using the dupCheck object, for which the keys would then be outputted
 
 var permutateString = function(str){
 	var count = 0;
 	var output =[];
 	var prefix = "";
+	var dupCheck = {};
 	var permString  = function(str, prefix){
 		if(str===""){
-			output.push(prefix);
-			count++;
+			if(dupCheck[prefix]===undefined){
+				dupCheck[prefix] = 1;
+				output.push(prefix);
+				count++;
+			}
 			return;
 		}
 		for(var i = 0; i<str.length; i++){
@@ -25,7 +36,13 @@ var permutateString = function(str){
 
 var string = "abc";
 var string2 = "abcd";
-var string3 = "aaaa";
+var string_dup = "abb";
+var string_dup2 = "bbb";
+var string_dup3 = "abcb";
+var string_dup4 = "aaaa";
 console.log(permutateString(string));
 console.log(permutateString(string2));
-console.log(permutateString(string3))
+console.log(permutateString(string_dup));
+console.log(permutateString(string_dup2));
+console.log(permutateString(string_dup3));
+console.log(permutateString(string_dup4));
