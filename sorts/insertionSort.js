df97598d1aefa2225e314b0f03121f05c6b727d1
Carol St.Louis <stlouis_c@yahoo.com>
2015-11-03 01:48:03 -0800

    tidied up console logs for insertion sort methods

--- sorts/insertionSort.js
+++ sorts/insertionSort.js
@@ -1,7 +1,7 @@
 // Given: a sorted array that has had a new element pushed to it without concern for sort-order, move the new element into place and return the sorted array
 
 function insertionSortEasy(arr){
-	// console.log(arr);
+	console.log("\r\n Input:", arr);
 	var temp = arr[arr.length - 1];
 	var pointer = arr.length - 1;
 	while(arr[pointer-1]>temp && pointer>0){
@@ -13,17 +13,15 @@ function insertionSortEasy(arr){
 	// console.log(arr);
 	return arr;
 }
-
-console.log('output:',insertionSortEasy([2, 4, 6, 8, 3]));
+console.log('Output:',insertionSortEasy([2, 4, 6, 8, 3]));
 
 
 // Given: an unsorted array, sort its constituent elements into order, starting at index 0, creating a sorted section and then one-by-one shifting the first element in the unsorted part of the array into its correct position within the array.
 
 function insertionSortFull(arr){
-	console.log(arr);
+	console.log("\r\n Input:", arr);
 	for(var i = 1; i<arr.length; i++){
 		var temp = arr[i];
-		console.log(temp);
 		var pointer = i;
 		while(arr[pointer-1]>temp && pointer>0){
 			arr[pointer] = arr[pointer-1];
@@ -36,15 +34,14 @@ function insertionSortFull(arr){
 	console.log(arr);
 	return arr;
 }
-
-console.log('output:',insertionSortFull([1, 4, 3, 5, 6, 2]));
-console.log('output:',insertionSortFull([1, 10, 4, 3, 5, 6, 2, -8, 2]));
+console.log('Output:',insertionSortFull([1, 4, 3, 5, 6, 2]));
+console.log('Output:',insertionSortFull([1, 10, 4, 3, 5, 6, 2, -8, 2]));
 
 
 // Given String Input, sort the numbers in the string -- input are space-separated numbers
 
 function insertionSortFullString(str){
-	console.log(str);
+	console.log("\r\n Input:", str);
 	var arr = str.split(" ");
 	// convert string to number
 	arr = arr.map(function(x){ return Number(x) });
@@ -62,5 +59,4 @@ function insertionSortFullString(str){
 	console.log(arr);
 	return arr;
 }
-
-console.log('output:',insertionSortFullString("8 4 23 3 91 5 6 89 2 3 9 12 -1 10 1"));
+console.log('Output:',insertionSortFullString("8 4 23 3 91 5 6 89 2 3 9 12 -1 10 1"));
