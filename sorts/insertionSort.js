1cd1c772f8165830594e213f566b1415abede127
Carol St.Louis <stlouis_c@yahoo.com>
2015-11-03 01:40:49 -0800

    insertion sort -- full array to sort

--- sorts/insertionSort.js
+++ sorts/insertionSort.js
@@ -16,3 +16,27 @@ function insertionSortEasy(arr){
 
 console.log('output:',insertionSortEasy([2, 4, 6, 8, 3]));
 
+
+// Given: an unsorted array, sort its constituent elements into order, starting at index 0, creating a sorted section and then one-by-one shifting the first element in the unsorted part of the array into its correct position within the array.
+
+function insertionSortFull(arr){
+	console.log(arr);
+	for(var i = 1; i<arr.length; i++){
+		var temp = arr[i];
+		console.log(temp);
+		var pointer = i;
+		while(arr[pointer-1]>temp && pointer>0){
+			arr[pointer] = arr[pointer-1];
+			pointer--;
+			// console.log(arr);
+		}
+		arr[pointer] = temp;
+		console.log(arr);
+	}
+	console.log(arr);
+	return arr;
+}
+
+console.log('output:',insertionSortFull([1, 4, 3, 5, 6, 2]));
+console.log('output:',insertionSortFull([1, 10, 4, 3, 5, 6, 2, -8, 2]));
+
